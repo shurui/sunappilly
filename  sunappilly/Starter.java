6d0a2cef00198acbd405c46fa2a1f5fcee09f67e
jamesatha@gmail.com <jamesatha@gmail.com@d349d4a8-5e2d-4b21-9363-d1e87a5a9a37>
2011-05-21 07:39:15 +0000

    main

---
+++  sunappilly/Starter.java
@@ -0,0 +1,15 @@
+import javax.swing.text.BadLocationException;
+
+
+public class Starter {
+
+	/**
+	 * @param args
+	 * @throws BadLocationException 
+	 */
+	public static void main(String[] args) throws BadLocationException {
+		// TODO Auto-generated method stub
+		FrontEnd frontEnd = new FrontEnd();
+	}
+
+}
